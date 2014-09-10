d2886cc57f183bde55639f435f2ce3f56e043fa9
Vasiliy Pedak <truvazia@gmail.com>
2014-09-10 21:35:18 +0300

    Removed temporary files and added to ignore

--- common/ui/ext/build/temp/development/ElectoralGroups/sass/config.rb
+++
@@ -1,3 +0,0 @@
-require '../../../../../ext/packages/ext-theme-base/sass/utils.rb'
-require '../../../../../ElectoralGroups/sass/config.rb'
-cache_path = '/var/www/aes/common/ui/ext/build/.sass-cache'
--- common/ui/ext/build/temp/production/ElectoralGroups/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /var/www/aes/common/ui/ext/ElectoralGroups/app.js
